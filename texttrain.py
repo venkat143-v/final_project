de24ef2313ddc7222ac58ba2ed2310e9a13e1823
Sai Venkat Javvadi <81428675+venkat143-v@users.noreply.github.com>
2024-04-27 11:10:14 +0530

    Create texttrain.py

---
+++ texttrain.py
@@ -0,0 +1,73 @@
+import json 
+import numpy as np 
+import tensorflow as tf
+from tensorflow import keras
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense, Embedding, SimpleRNN
+from tensorflow.keras.preprocessing.text import Tokenizer
+from tensorflow.keras.preprocessing.sequence import pad_sequences
+from sklearn.preprocessing import LabelEncoder
+
+# Load the JSON data
+with open(r'newf.json') as file:
+    data = json.load(file)
+    
+training_sentences = []
+training_labels = []
+labels = []
+responses = []
+
+# Extract data from JSON
+for intent in data['intents']:
+    for pattern in intent['patterns']:
+        training_sentences.append(pattern)
+        training_labels.append(intent['tag'])
+    responses.append(intent['responses'])
+    
+    if intent['tag'] not in labels:
+        labels.append(intent['tag'])
+        
+num_classes = len(labels)
+
+# Encode labels using LabelEncoder
+lbl_encoder = LabelEncoder()
+lbl_encoder.fit(training_labels)
+training_labels = lbl_encoder.transform(training_labels)
+
+# Tokenization and Padding
+tokenizer = Tokenizer()
+tokenizer.fit_on_texts(training_sentences)
+word_index = tokenizer.word_index
+sequences = tokenizer.texts_to_sequences(training_sentences)
+padded_sequences = pad_sequences(sequences, padding='post')
+
+# Define the RNN model
+model = Sequential()
+model.add(Embedding(len(word_index) + 1, 128))  # Using word_index size + 1 for embedding
+model.add(SimpleRNN(128))
+model.add(Dense(64, activation='relu'))
+model.add(Dense(num_classes, activation='softmax'))
+
+# Compile the model
+model.compile(loss='sparse_categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+
+# Print model summary
+model.summary()
+
+# Train the model
+epochs = 500  # You can adjust this number based on your needs
+history = model.fit(padded_sequences, np.array(training_labels), epochs=epochs)
+
+# Save the trained model
+model.save("newf_model")
+
+
+
+import pickle
+# Save the fitted tokenizer
+with open('tokenizer.pickle', 'wb') as handle:
+    pickle.dump(tokenizer, handle, protocol=pickle.HIGHEST_PROTOCOL)
+    
+# Save the fitted label encoder
+with open('label_encoder.pickle', 'wb') as ecn_file:
+    pickle.dump(lbl_encoder, ecn_file, protocol=pickle.HIGHEST_PROTOCOL)
