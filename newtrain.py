b9f038f72fea687ec3d89d75b8a8488b0b881925
Sai Venkat Javvadi <81428675+venkat143-v@users.noreply.github.com>
2024-04-27 11:05:33 +0530

    texttrain.py

--- newtrain.py
+++ newtrain.py
@@ -1,14 +1,21 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Apr 27 10:22:33 2024
+
+@author: Lenovo
+"""
 
 import json 
 import numpy as np 
 import tensorflow as tf
 from tensorflow import keras
-from tensorflow.keras.models import Sequential, SimpleRNN, Dense
-from tensorflow.keras.layers import Dense, Embedding, GlobalAveragePooling1D
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense, Embedding, SimpleRNN
 from tensorflow.keras.preprocessing.text import Tokenizer
 from tensorflow.keras.preprocessing.sequence import pad_sequences
 from sklearn.preprocessing import LabelEncoder
 
+# Load the JSON data
 with open(r'newf.json') as file:
     data = json.load(file)
     
@@ -17,7 +24,7 @@ training_labels = []
 labels = []
 responses = []
 
-
+# Extract data from JSON
 for intent in data['intents']:
     for pattern in intent['patterns']:
         training_sentences.append(pattern)
@@ -29,49 +36,45 @@ for intent in data['intents']:
         
 num_classes = len(labels)
 
+# Encode labels using LabelEncoder
 lbl_encoder = LabelEncoder()
 lbl_encoder.fit(training_labels)
 training_labels = lbl_encoder.transform(training_labels)
 
-vocab_size = 1000
-embedding_dim = 16
-max_len = 20
-oov_token = "<OOV>"
-
-tokenizer = Tokenizer(num_words=vocab_size, oov_token=oov_token)
+# Tokenization and Padding
+tokenizer = Tokenizer()
 tokenizer.fit_on_texts(training_sentences)
 word_index = tokenizer.word_index
 sequences = tokenizer.texts_to_sequences(training_sentences)
-padded_sequences = pad_sequences(sequences, truncating='post', maxlen=max_len)
+padded_sequences = pad_sequences(sequences, padding='post')
 
+# Define the RNN model
 model = Sequential()
-model.add(Embedding(vocab_size, embedding_dim, input_length=max_len))
-model.add(SimpleRNN(16))  # Simple RNN layer with 16 units
-model.add(Dense(16, activation='relu'))
-model.add(Dense(16, activation='relu'))
-model.add(Dense(16, activation='relu'))
+model.add(Embedding(len(word_index) + 1, 128))  # Using word_index size + 1 for embedding
+model.add(SimpleRNN(128))
+model.add(Dense(64, activation='relu'))
 model.add(Dense(num_classes, activation='softmax'))
 
+# Compile the model
+model.compile(loss='sparse_categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
 
-
-model.compile(loss='sparse_categorical_crossentropy', 
-              optimizer='adam', metrics=['accuracy'])
-
+# Print model summary
 model.summary()
-epochs = 1000
+
+# Train the model
+epochs = 500  # You can adjust this number based on your needs
 history = model.fit(padded_sequences, np.array(training_labels), epochs=epochs)
 
-# to save the trained model
+# Save the trained model
+model.save("newf_model")
 
-model.save_weights("newf_model")
+
 
 import pickle
-
-# to save the fitted tokenizer
+# Save the fitted tokenizer
 with open('tokenizer.pickle', 'wb') as handle:
     pickle.dump(tokenizer, handle, protocol=pickle.HIGHEST_PROTOCOL)
     
-# to save the fitted label encoder
+# Save the fitted label encoder
 with open('label_encoder.pickle', 'wb') as ecn_file:
     pickle.dump(lbl_encoder, ecn_file, protocol=pickle.HIGHEST_PROTOCOL)
-    
